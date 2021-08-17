196d4c5688f2717f9b81b9839055105c6d1d06c8
Syed Aamir <81068983+KODESAM@users.noreply.github.com>
2021-08-18 01:31:48 +0400

    docker_container_target.sh

---
+++ advance_lab/docker_container_target.sh
@@ -0,0 +1,16 @@
+Run the container:
+
+docker run -d mmumshad/ubuntu-ssh-enabled
+
+Identify the Internal IP
+
+docker inspect <container-id-name>
+
+SSH
+
+ssh <container-ip>
+
+Username: root
+
+Password: Passw0rd
+
